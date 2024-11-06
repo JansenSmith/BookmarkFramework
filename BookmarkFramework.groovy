c279b3349cf9a9348a921c5c3077136cca1b6893
Jansen Smith <jansen@bstarengineering.com>
2024-11-06 10:09:54 -0500

    adding the import

--- BookmarkFramework.groovy
+++ BookmarkFramework.groovy
@@ -1,5 +1,13 @@
 
 
+// Load an STL file from a git repo
+// Loading a local file also works here
+File stlFile = ScriptingEngine.fileFromGit(
+	"https://github.com/JansenSmith/BookmarkFramework.git",
+	"source_stl/pirates1_Front_68x205_S_C.stl");
+// Load the .CSG from the disk and cache it in memory
+CSG source  = Vitamins.get(stlFile);
+
 def width = 30
 def height = 120
 def thickness = 7
