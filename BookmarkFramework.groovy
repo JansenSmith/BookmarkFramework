c9211dfe4252aebea81f9eec4d6c4f90e4926bc9
Jansen Smith <jansen@bstarengineering.com>
2025-01-11 13:45:03 -0500

    added comments and simplified diff and returned bookmark

--- BookmarkFramework.groovy
+++ BookmarkFramework.groovy
@@ -5,6 +5,7 @@ import eu.mihosoft.vrl.v3d.RoundedCube
 import eu.mihosoft.vrl.v3d.Transform
 
 
+println "Loading pirate STL"
 // Load an STL file from a git repo
 // Loading a local file also works here
 File stlFile = ScriptingEngine.fileFromGit(
@@ -12,10 +13,13 @@ File stlFile = ScriptingEngine.fileFromGit(
 	"source_stl/pirates3_Front_68x205_S_C_meshlab.stl");
 // Load the .CSG from the disk and cache it in memory
 CSG source  = Vitamins.get(stlFile);
-CSG placeholder = new Cube(68.4,205,3).toCSG()
+println "STL loaded"
+
+//CSG placeholder = new Cube(68.4,205,3).toCSG()
 //source = placeholder
 source = source.toZMin()
 
+println "Prepping cutter"
 def width = source.getTotalX()
 def height = source.getTotalY()
 def thickness = source.getTotalZ()
@@ -37,11 +41,25 @@ def eyehole = new Cylinder(eyehole_diameter/2,thickness,(int)32).toCSG()
 					.movey(bookmark.getMaxY())
 					.movey(-eyehole_fromtop)
 bookmark = bookmark.difference(eyehole)
-bookmark = bookmark.intersect(source)
+
+println "Running cut"
+bookmark = source.difference(bookmark)
+//nullspace = source.difference(bookmark).movez(5)
+//bookmark = bookmark.intersect(source)
+
+println "Configuring returned CSGs"
+source = source.setColor(javafx.scene.paint.Color.BLUE)
+			.setName("bookmark_pirate3_source")
+			.addAssemblyStep(0, new Transform())
+			.setManufacturing({ toMfg ->
+				return toMfg
+						//.rotx(180)// fix the orientation
+						//.toZMin()//move it down to the flat surface
+			})
 
 bookmark = bookmark.setColor(javafx.scene.paint.Color.PINK)
 			.setName("bookmark_pirate3")
-			.addAssemblyStep(0, new Transform())
+			.addAssemblyStep(0, new Transform().movez(5))
 			.setManufacturing({ toMfg ->
 				return toMfg
 						//.rotx(180)// fix the orientation
